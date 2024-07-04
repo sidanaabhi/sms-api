7363c4e64c395b303ed1ba3108fc2243971588bd
ABHISHEK SIDANA <abhishek.sidana@daimlertruck.com>
2024-07-04 13:04:09 +0530

    code .

--- sms.js
+++ sms.js
@@ -1,4 +1,9 @@
 // Load the Twilio module
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+global.require = require; //this will make require at the global scobe and treat it like the original require
+
 const twilio = require('twilio');
 const AWS = require('aws-sdk');
 const b64 = require('base64-js');
